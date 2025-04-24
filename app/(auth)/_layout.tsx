b8c2ede32714ffb476b9a8af72691edb176549bf
anilyavas <anil.yavas99@gmail.com>
2025-04-24 15:44:30 +0300

    Removed unused Stack.Screen components from AuthLayout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -13,10 +13,5 @@ export default function AuthLayout() {
     return <ActivityIndicator />;
   }
 
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="sign-in" />
-      <Stack.Screen name="sign-up" />
-    </Stack>
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
