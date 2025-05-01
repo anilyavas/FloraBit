c1857c05df9c8a60795d3761ae274ce1e5088677
anilyavas <anil.yavas99@gmail.com>
2025-05-01 19:58:43 +0300

    Update dependencies

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,16 +1,11 @@
 import { useAuth } from '@clerk/clerk-expo';
 import { Redirect, Stack } from 'expo-router';
-import { ActivityIndicator } from 'react-native';
 
 export default function AuthLayout() {
-  const { isSignedIn, isLoaded } = useAuth();
+  const { isSignedIn } = useAuth();
 
   if (isSignedIn) {
     return <Redirect href="/(tabs)" />;
-  }
-
-  if (!isLoaded) {
-    return <ActivityIndicator />;
   }
 
   return <Stack screenOptions={{ headerShown: false }} />;
